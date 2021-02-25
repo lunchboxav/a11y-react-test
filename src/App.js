75db569b6f6e6214416a363dc0d11f512811d4ca
Adityo Pratomo <adityo@tetrate.io>
2021-02-25 07:48:26 +0700

    remove unused variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import About from "./views/About";
 import Notes from "./views/Notes";
