7f8bebd1eaf94b7ba776dc4e32c986a9d33797ac
Adityo Pratomo <adityo@tetrate.io>
2021-02-27 09:55:02 +0700

    styling form

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
+import { styled } from "../stitches.config";
+
+const Input = styled("input", {
+  margin: "8px 0 16px",
+  padding: "12px 20px",
+  width: "480px",
+});
+
+const TextArea = styled("textarea", {
+  margin: "8px 0 16px",
+  padding: "12px 20px",
+  width: "480px",
+  height: "320px",
+  fontFamily: 'Arial'
+});
 
 function NoteForm(props) {
   const [form, setForm] = useState({
-    title: '',
-    content: ''
+    title: "",
+    content: "",
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(form);
+    setForm({
+      title: "",
+      content: "",
+    });
   };
 
   const handleUpdate = (e) => {
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -26,21 +45,21 @@ function NoteForm(props) {
     >
       <label>Title</label>
       <br />
-      <input
+      <Input
         name="title"
         type="text"
         value={form.title}
         onChange={handleUpdate}
-      ></input>
+      ></Input>
       <br />
       <label>Note</label>
       <br />
-      <textarea
+      <TextArea
         name="content"
         type="text"
         value={form.content}
         onChange={handleUpdate}
-      ></textarea>
+      ></TextArea>
       <br />
       <input type="submit" value="Submit" />
     </form>
